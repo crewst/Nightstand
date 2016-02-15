a9810948fc5a8487e05b895b4521ab5a3cd006fe
Tommy Crews <twcrews96@gmail.com>
2016-02-15 14:40:22 -0600

    Added behavior for ClockLabel. Added SettingsButton and created new SettingsViewController. Cosmetic improvements to ClockLabel.

--- Nightstand/ViewController.swift
+++ Nightstand/ViewController.swift
@@ -8,10 +8,42 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
-
+	
+	
     @IBOutlet weak var ClockLabel: UILabel!
+	@IBOutlet weak var SettingsButton: UIButton!
+	
+	
+	var timer = NSTimer()
+	var dateFormatter = NSDateFormatter()
+	
 
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+				dateFormatter.timeStyle = .ShortStyle
+		
+		let timeString = "\(dateFormatter.stringFromDate(NSDate()))"
+		
+		ClockLabel.text = timeString
+		
+		// Do any additional setup after loading the view, typically from a nib.
+		
+		self.timer = NSTimer.scheduledTimerWithTimeInterval(1.0,
+			target: self,
+			selector: Selector("tick"),
+			userInfo: nil,
+			repeats: true)
+		UIScreen.mainScreen().brightness = CGFloat(0.0)
+	}
+	
+	@objc func tick() {
+		ClockLabel.text = NSDateFormatter.localizedStringFromDate(NSDate(),
+			dateStyle: .NoStyle,
+			timeStyle: .ShortStyle)
+	}
 
 }
-
