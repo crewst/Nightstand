5eaea88b02c6febb2dd511f6249625916305db6c
Mac Lab <maclab@stf259-c8.student.swosu.edu>
2016-02-25 15:19:03 -0600

    Initial branch commit

---
+++ Nightstand/TrafficView.swift
@@ -0,0 +1,20 @@
+//
+//  TrafficView.swift
+//  Nightstand
+//
+//  Created by Mac Lab on 2/25/16.
+//  Copyright © 2016 SWOSU Software Engineering. All rights reserved.
+//
+
+import UIKit
+
+class TrafficView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}
